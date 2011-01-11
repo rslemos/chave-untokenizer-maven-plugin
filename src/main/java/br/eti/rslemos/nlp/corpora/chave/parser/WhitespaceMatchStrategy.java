4818894d956d5f726e628cb7a9a5638e768c1c39
R. Lemos <rslemos@gmail.com>
2011-01-10 22:01:13 -0200

    matcher de espaço em branco (não consome nenhuma entrada da CG... consome apenas caracteres do texto)

---
+++ src/main/java/br/eti/rslemos/nlp/corpora/chave/parser/WhitespaceMatchStrategy.java
@@ -0,0 +1,34 @@
+package br.eti.rslemos.nlp.corpora.chave.parser;
+
+import static java.lang.Character.isWhitespace;
+
+import java.nio.BufferUnderflowException;
+import java.nio.CharBuffer;
+import java.util.List;
+
+import br.eti.rslemos.nlp.corpora.chave.parser.Parser.Entry;
+
+public class WhitespaceMatchStrategy implements MatchStrategy {
+
+	public MatchResult match(final CharBuffer buffer, List<Entry<String, String>> cg) throws BufferUnderflowException {
+		int k = 0;
+		
+		try {
+			while(isWhitespace(buffer.charAt(k)))
+				k++;
+		} catch (IndexOutOfBoundsException e) {}
+		
+		if (k > 0) {
+			final int k1 = k; 
+			return new MatchResult() {
+				public void apply(Handler handler) {
+					char[] cs = new char[k1];
+					buffer.get(cs);
+					handler.characters(cs);
+				}
+			};
+		} else
+			return null;
+	}
+
+}
---
+++ src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/WhitespaceMatchStrategyUnitTest.java
@@ -0,0 +1,36 @@
+package br.eti.rslemos.nlp.corpora.chave.parser;
+
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.eti.rslemos.nlp.corpora.chave.parser.Parser.Entry;
+
+public class WhitespaceMatchStrategyUnitTest extends AbstractMatchStrategyUnitTest {
+	@Before
+	public void setUp() {
+		super.setUp(new WhitespaceMatchStrategy());
+	}
+	
+	@Test
+	public void testWhitespace() throws Exception {
+		@SuppressWarnings("unchecked")
+		List<Entry<String, String>> cg = Arrays.asList();
+		matchAndApply(cg, "   \t\t   \t   abcd");
+
+		verify(handler).characters("   \t\t   \t   ".toCharArray());
+	}
+	
+	@Test
+	public void testFinalWhitespace() throws Exception {
+		@SuppressWarnings("unchecked")
+		List<Entry<String, String>> cg = Arrays.asList();
+		matchAndApply(cg, "   \t\t   \t   ");
+
+		verify(handler).characters("   \t\t   \t   ".toCharArray());
+	}
+}
