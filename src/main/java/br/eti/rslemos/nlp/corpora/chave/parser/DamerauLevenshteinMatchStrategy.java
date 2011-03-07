d960d308a09054db7b29811c383c92ec7b24eb3b
R. Lemos (oslo) <rslemos@gmail.com>
2011-03-07 01:08:18 -0300

    refatoração da distância damerau-levenshtein: separação inicial da classe AdaptativeDamerauLevenshteinDistance

--- src/main/java/br/eti/rslemos/nlp/corpora/chave/parser/DamerauLevenshteinMatchStrategy.java
+++ src/main/java/br/eti/rslemos/nlp/corpora/chave/parser/DamerauLevenshteinMatchStrategy.java
@@ -49,38 +49,62 @@ public class DamerauLevenshteinMatchStrategy extends OldStyleMatchStrategy {
 	}
 
 	private static int levenshteinDistance(char[] key, char[] text) {
-		int d0[] = null;
-		int d1[] = null;
-		int d2[] = null;
-
-		d2 = new int[key.length + 1];
-
-		for(int i=0; i<=key.length; i++) {
-			d2[i] = i;
-		}
-		
-		for(int j=1; j<=text.length; j++) {
-			d0 = d1;
-			d1 = d2;
-			d2 = new int[key.length + 1];
+		class AdaptativeDamerauLevenshteinDistance {
+			private int d0[] = null;
+			private int d1[] = null;
+			private int d2[] = null;
 			
-			d2[0] = j;
+			private int j = 1;
+			private char c0;
 			
-			for(int i=1; i<=key.length; i++) {
-				int cost;
+			private final char[] key;
+			
+			private AdaptativeDamerauLevenshteinDistance(char[] key) {
+				this.key = key;
+				d2 = new int[key.length + 1];
 				
-				if (key[i-1] == text[j-1])
-					cost = 0;
-				else
-					cost = 1;
+				for(int i=0; i<=key.length; i++) {
+					d2[i] = i;
+				}
+			}
+			
+			int appendChar(char c1) {
+				d0 = d1;
+				d1 = d2;
+				d2 = new int[key.length + 1];
 				
-				d2[i] = Math.min(Math.min(d2[i-1] + 1, d1[i] + 1), d1[i-1] + cost);
+				d2[0] = j;
+				
+				for(int i=1; i<=key.length; i++) {
+					int cost;
+					
+					if (key[i-1] == c1)
+						cost = 0;
+					else
+						cost = 1;
+					
+					d2[i] = Math.min(Math.min(d2[i-1] + 1, d1[i] + 1), d1[i-1] + cost);
 
-				if (i > 1 && j > 1 && key[i-1] == text[j-2] && key[i-2] == text[j-1])
-					d2[i] = Math.min(d2[i], d0[i-2] + cost);
+					if (i > 1 && j > 1 && key[i-1] == c0 && key[i-2] == c1)
+						d2[i] = Math.min(d2[i], d0[i-2] + cost);
+				}
+				
+				c0 = c1;
+				j++;
+				
+				return d2[key.length];
 			}
 		}
+
+		AdaptativeDamerauLevenshteinDistance c = new AdaptativeDamerauLevenshteinDistance(key);
 		
-		return d2[key.length];
+		int distance = -1;
+		
+		for(int j=1; j<=text.length; j++) {
+			distance = c.appendChar(text[j-1]);
+			System.out.println(distance);
+		}
+		
+		return distance;
 	}
 }
