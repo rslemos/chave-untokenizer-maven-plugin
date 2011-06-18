7f2f231df6d4e2f87c23868203e589091a717b5d
R. Lemos <rslemos@gmail.com>
2011-06-18 14:27:03 -0300

    retirada de comentário inútil (em método que ainda nem existe) e de @deprecation em método que ainda está sendo usado (e será por muito tempo, pelo menos até acabar o NewUntokenizer)

--- src/main/java/br/eti/rslemos/nlp/corpora/chave/parser/MatchStrategy.java
+++ src/main/java/br/eti/rslemos/nlp/corpora/chave/parser/MatchStrategy.java
@@ -6,18 +6,5 @@ import java.util.Set;
 public interface MatchStrategy {
 	void setData(TextMatcher matcher, List<String> cg);
 	
-	/**
-	 * Try to match the <code>i</code>-th key (and a few following it) over the
-	 * text possibly starting at <code>k</code>.
-	 * 
-	 * @param k
-	 * @param i
-	 * 
-	 * @return the nearest match starting at <code>k</code> if one such match
-	 * exists; <code>null</code> otherwise.
-	 */
-	//Match match(int k, int i);
-	
-	@Deprecated
 	Set<Match> matchAll();
 }
