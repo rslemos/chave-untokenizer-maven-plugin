ccd9a226d857486ee586a697a095b9a577b5ccfe
R. Lemos (oslo) <rslemos@gmail.com>
2011-06-19 17:18:33 -0300

    contrações "com + x" com case sensitiveness

--- src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/ContractionComMatchStrategyUnitTest.java
+++ src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/ContractionComMatchStrategyUnitTest.java
@@ -11,6 +11,7 @@ public class ContractionComMatchStrategyUnitTest extends AbstractMatchStrategyUn
 		super(new ContractionComMatchStrategy());
 	}
 	
+	// com + x
 	@Test
 	public void testContraction_com_mim() throws Exception {
 		cg.add("com");
@@ -68,6 +69,128 @@ public class ContractionComMatchStrategyUnitTest extends AbstractMatchStrategyUn
 		
 		verifyMatches(match(0, "convosco".length(), span(0, "con".length(), 0), 
 				span("con".length(), "convosco".length(), 1)
+			));
+	}
+	
+	// Com + x
+	@Test
+	public void testContraction_Com_mim() throws Exception {
+		cg.add("Com");
+		cg.add("mim");
+		
+		runOver("Comigo");
+		
+		verifyMatches(match(0, "Comigo".length(), span(0, "Com".length(), 0),  
+				span("Co".length(), "Comigo".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_Com_ti() throws Exception {
+		cg.add("Com");
+		cg.add("ti");
+		
+		runOver("Contigo");
+		
+		verifyMatches(match(0, "Contigo".length(), span(0, "Con".length(), 0), 
+				span("Con".length(), "Contigo".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_Com_si() throws Exception {
+		cg.add("Com");
+		cg.add("si");
+		
+		runOver("Consigo");
+		
+		verifyMatches(match(0, "Consigo".length(), span(0, "Con".length(), 0), 
+				span("Con".length(), "Consigo".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_Com_nos() throws Exception {
+		cg.add("Com");
+		cg.add("nos");
+		
+		runOver("Conosco");
+		
+		verifyMatches(match(0, "Conosco".length(), span(0, "Con".length(), 0),  
+				span("Co".length(), "Conosco".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_Com_vos() throws Exception {
+		cg.add("Com");
+		cg.add("vos");
+		
+		runOver("Convosco");
+		
+		verifyMatches(match(0, "Convosco".length(), span(0, "Con".length(), 0), 
+				span("Con".length(), "Convosco".length(), 1)
+			));
+	}
+	
+	// COM + X
+	@Test
+	public void testCONtraction_COM_MIM() throws Exception {
+		cg.add("COM");
+		cg.add("MIM");
+		
+		runOver("COMIGO");
+		
+		verifyMatches(match(0, "COMIGO".length(), span(0, "COM".length(), 0),  
+				span("CO".length(), "COMIGO".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testCONtraction_COM_TI() throws Exception {
+		cg.add("COM");
+		cg.add("TI");
+		
+		runOver("CONTIGO");
+		
+		verifyMatches(match(0, "CONTIGO".length(), span(0, "CON".length(), 0), 
+				span("CON".length(), "CONTIGO".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testCONtraction_COM_SI() throws Exception {
+		cg.add("COM");
+		cg.add("SI");
+		
+		runOver("CONSIGO");
+		
+		verifyMatches(match(0, "CONSIGO".length(), span(0, "CON".length(), 0), 
+				span("CON".length(), "CONSIGO".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testCONtraction_COM_NOS() throws Exception {
+		cg.add("COM");
+		cg.add("NOS");
+		
+		runOver("CONOSCO");
+		
+		verifyMatches(match(0, "CONOSCO".length(), span(0, "CON".length(), 0),  
+				span("CO".length(), "CONOSCO".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testCONtraction_COM_VOS() throws Exception {
+		cg.add("COM");
+		cg.add("VOS");
+		
+		runOver("CONVOSCO");
+		
+		verifyMatches(match(0, "CONVOSCO".length(), span(0, "CON".length(), 0), 
+				span("CON".length(), "CONVOSCO".length(), 1)
 			));
 	}
 }
