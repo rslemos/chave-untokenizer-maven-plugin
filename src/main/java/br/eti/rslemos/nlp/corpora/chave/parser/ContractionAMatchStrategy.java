3add812cb55d2d9da69ba95c5b4516ad2d05750c
R. Lemos (oslo) <rslemos@gmail.com>
2011-06-19 16:58:30 -0300

    contrações "a + x" com case sensitiveness

--- src/main/java/br/eti/rslemos/nlp/corpora/chave/parser/ContractionAMatchStrategy.java
+++ src/main/java/br/eti/rslemos/nlp/corpora/chave/parser/ContractionAMatchStrategy.java
@@ -3,6 +3,27 @@ package br.eti.rslemos.nlp.corpora.chave.parser;
 public class ContractionAMatchStrategy extends AbstractContractionMatchStrategy {
 
 	private static final ContractionTemplate[] TEMPLATES = {
+			// A + X
+			new ContractionTemplate("A", "A", "À", 1, 0),
+			new ContractionTemplate("A", "AQUELA", "ÀQUELA", 1, 0),
+			new ContractionTemplate("A", "AQUELAS", "ÀQUELAS", 1, 0),
+			new ContractionTemplate("A", "AQUELE", "ÀQUELE", 1, 0),
+			new ContractionTemplate("A", "AQUELES", "ÀQUELES", 1, 0),
+			new ContractionTemplate("A", "AQUILO", "ÀQUILO", 1, 0),
+			new ContractionTemplate("A", "AS", "ÀS", 1, 0),
+			new ContractionTemplate("A", "O", "AO", 1, 1),
+			new ContractionTemplate("A", "OS", "AOS", 1, 1),
+			// A + x
+			new ContractionTemplate("A", "a", "À", 1, 0),
+			new ContractionTemplate("A", "aquela", "Àquela", 1, 0),
+			new ContractionTemplate("A", "aquelas", "Àquelas", 1, 0),
+			new ContractionTemplate("A", "aquele", "Àquele", 1, 0),
+			new ContractionTemplate("A", "aqueles", "Àqueles", 1, 0),
+			new ContractionTemplate("A", "aquilo", "Àquilo", 1, 0),
+			new ContractionTemplate("A", "as", "Às", 1, 0),
+			new ContractionTemplate("A", "o", "Ao", 1, 1),
+			new ContractionTemplate("A", "os", "Aos", 1, 1),
+			// a + x
 			new ContractionTemplate("a", "a", "à", 1, 0),
 			new ContractionTemplate("a", "aquela", "àquela", 1, 0),
 			new ContractionTemplate("a", "aquelas", "àquelas", 1, 0),
--- src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/ContractionAMatchStrategyUnitTest.java
+++ src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/ContractionAMatchStrategyUnitTest.java
@@ -11,6 +11,7 @@ public class ContractionAMatchStrategyUnitTest extends AbstractMatchStrategyUnit
 		super(new ContractionAMatchStrategy());
 	}
 	
+	// a + x
 	@Test
 	public void testContraction_a_o() throws Exception {
 		cg.add("a");
@@ -120,14 +121,14 @@ public class ContractionAMatchStrategyUnitTest extends AbstractMatchStrategyUnit
 	}
 	
 	@Test
-	public void testExpressionContraction_Quanto_a_as() throws Exception {
-		cg.add("Quanto=a");
+	public void testExpressionContraction_auanto_a_as() throws Exception {
+		cg.add("quanto=a");
 		cg.add("as");
 		
-		runOver("Quanto às");
+		runOver("quanto às");
 		
-		verifyMatches(match(0, "Quanto às".length(), span(0, "Quanto à".length(), 0),  
-			span("Quanto ".length(), "Quanto às".length(), 1)
+		verifyMatches(match(0, "quanto às".length(), span(0, "quanto à".length(), 0),  
+			span("quanto ".length(), "quanto às".length(), 1)
 		));
 	}
 	
@@ -164,6 +165,320 @@ public class ContractionAMatchStrategyUnitTest extends AbstractMatchStrategyUnit
 		
 		verifyMatches(match(0, "devido aos quais".length(), span(0, "devido a".length(), 0), 
 				span("devido a".length(), "devido aos quais".length(), 1)
+			));
+	}
+	
+	// A + x
+	@Test
+	public void testContraction_A_o() throws Exception {
+		cg.add("A");
+		cg.add("o");
+		
+		runOver("Ao");
+		
+		verifyMatches(match(0, 2, span(0, 1, 0), 
+				span(1, 2, 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_a() throws Exception {
+		cg.add("A");
+		cg.add("a");
+		
+		runOver("À");
+		
+		verifyMatches(match(0, 1, span(0, 1, 0), 
+				span(0, 1, 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_os() throws Exception {
+		cg.add("A");
+		cg.add("os");
+		
+		runOver("Aos");
+		
+		verifyMatches(match(0, 3, span(0, 1, 0), 
+				span(1, 3, 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_as() throws Exception {
+		cg.add("A");
+		cg.add("as");
+		
+		runOver("Às");
+		
+		verifyMatches(match(0, 2, span(0, 1, 0),
+				span(0, 2, 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_aquele() throws Exception {
+		cg.add("A");
+		cg.add("aquele");
+		
+		runOver("Àquele");
+		
+		verifyMatches(match(0, "Àquele".length(), span(0, 1, 0),
+				span(0, "Àquele".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_aquela() throws Exception {
+		cg.add("A");
+		cg.add("aquela");
+		
+		runOver("Àquela");
+		
+		verifyMatches(match(0, "Àquela".length(), span(0, 1, 0),
+				span(0, "Àquela".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_aqueles() throws Exception {
+		cg.add("A");
+		cg.add("aqueles");
+		
+		runOver("Àqueles");
+		
+		verifyMatches(match(0, "Àqueles".length(), span(0, 1, 0),
+				span(0, "Àqueles".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_aquelas() throws Exception {
+		cg.add("A");
+		cg.add("aquelas");
+		
+		runOver("Àquelas");
+		
+		verifyMatches(match(0, "Àquelas".length(), span(0, 1, 0),
+				span(0, "Àquelas".length(), 1)
+			));
+	}
+
+	@Test
+	public void testContraction_A_aquilo() throws Exception {
+		cg.add("A");
+		cg.add("aquilo");
+		
+		runOver("Àquilo");
+		
+		verifyMatches(match(0, "Àquilo".length(), span(0, 1, 0),
+				span(0, "Àquilo".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testExpressionContraction_Quanto_a_as() throws Exception {
+		cg.add("Quanto=a");
+		cg.add("as");
+		
+		runOver("Quanto às");
+		
+		verifyMatches(match(0, "Quanto às".length(), span(0, "Quanto à".length(), 0),  
+			span("Quanto ".length(), "Quanto às".length(), 1)
+		));
+	}
+	
+	@Test
+	public void testExpressionContraction_A_as_quais() throws Exception {
+		cg.add("A");
+		cg.add("as=quais");
+		
+		runOver("Às quais");
+		
+		verifyMatches(match(0, "Às quais".length(), span(0, 1, 0),
+				span(0, "Às quais".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testExpressionContraction_Devido_a_a() throws Exception {
+		cg.add("Devido=a");
+		cg.add("a");
+		
+		runOver("Devido à");
+		
+		verifyMatches(match(0, "Devido à".length(), span(0, "Devido à".length(), 0), 
+				span("Devido ".length(), "Devido à".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testExpressionContraction_Devido_a_os_quais() throws Exception {
+		cg.add("Devido=a");
+		cg.add("os=quais");
+		
+		runOver("Devido aos quais");
+		
+		verifyMatches(match(0, "Devido aos quais".length(), span(0, "Devido a".length(), 0), 
+				span("Devido a".length(), "Devido aos quais".length(), 1)
+			));
+	}
+	
+	// A + X
+	@Test
+	public void testContraction_A_O() throws Exception {
+		cg.add("A");
+		cg.add("O");
+		
+		runOver("AO");
+		
+		verifyMatches(match(0, 2, span(0, 1, 0), 
+				span(1, 2, 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_A() throws Exception {
+		cg.add("A");
+		cg.add("A");
+		
+		runOver("À");
+		
+		verifyMatches(match(0, 1, span(0, 1, 0), 
+				span(0, 1, 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_OS() throws Exception {
+		cg.add("A");
+		cg.add("OS");
+		
+		runOver("AOS");
+		
+		verifyMatches(match(0, 3, span(0, 1, 0), 
+				span(1, 3, 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_AS() throws Exception {
+		cg.add("A");
+		cg.add("AS");
+		
+		runOver("ÀS");
+		
+		verifyMatches(match(0, 2, span(0, 1, 0),
+				span(0, 2, 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_AQUELE() throws Exception {
+		cg.add("A");
+		cg.add("AQUELE");
+		
+		runOver("ÀQUELE");
+		
+		verifyMatches(match(0, "ÀQUELE".length(), span(0, 1, 0),
+				span(0, "ÀQUELE".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_AQUELA() throws Exception {
+		cg.add("A");
+		cg.add("AQUELA");
+		
+		runOver("ÀQUELA");
+		
+		verifyMatches(match(0, "ÀQUELA".length(), span(0, 1, 0),
+				span(0, "ÀQUELA".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_AQUELES() throws Exception {
+		cg.add("A");
+		cg.add("AQUELES");
+		
+		runOver("ÀQUELES");
+		
+		verifyMatches(match(0, "ÀQUELES".length(), span(0, 1, 0),
+				span(0, "ÀQUELES".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testContraction_A_AQUELAS() throws Exception {
+		cg.add("A");
+		cg.add("AQUELAS");
+		
+		runOver("ÀQUELAS");
+		
+		verifyMatches(match(0, "ÀQUELAS".length(), span(0, 1, 0),
+				span(0, "ÀQUELAS".length(), 1)
+			));
+	}
+
+	@Test
+	public void testContraction_A_AQUILO() throws Exception {
+		cg.add("A");
+		cg.add("AQUILO");
+		
+		runOver("ÀQUILO");
+		
+		verifyMatches(match(0, "ÀQUILO".length(), span(0, 1, 0),
+				span(0, "ÀQUILO".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testExpressionContraction_QUANTO_A_AS() throws Exception {
+		cg.add("QUANTO=A");
+		cg.add("AS");
+		
+		runOver("QUANTO ÀS");
+		
+		verifyMatches(match(0, "QUANTO ÀS".length(), span(0, "QUANTO À".length(), 0),  
+			span("QUANTO ".length(), "QUANTO ÀS".length(), 1)
+		));
+	}
+	
+	@Test
+	public void testExpressionContraction_A_AS_QUAIS() throws Exception {
+		cg.add("A");
+		cg.add("AS=QUAIS");
+		
+		runOver("ÀS QUAIS");
+		
+		verifyMatches(match(0, "ÀS QUAIS".length(), span(0, 1, 0),
+				span(0, "ÀS QUAIS".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testExpressionContraction_DEVIDO_A_A() throws Exception {
+		cg.add("DEVIDO=A");
+		cg.add("A");
+		
+		runOver("DEVIDO À");
+		
+		verifyMatches(match(0, "DEVIDO À".length(), span(0, "DEVIDO À".length(), 0), 
+				span("DEVIDO ".length(), "DEVIDO À".length(), 1)
+			));
+	}
+	
+	@Test
+	public void testExpressionContraction_DEVIDO_A_OS_QUAIS() throws Exception {
+		cg.add("DEVIDO=A");
+		cg.add("OS=QUAIS");
+		
+		runOver("DEVIDO AOS QUAIS");
+		
+		verifyMatches(match(0, "DEVIDO AOS QUAIS".length(), span(0, "DEVIDO A".length(), 0), 
+				span("DEVIDO A".length(), "DEVIDO AOS QUAIS".length(), 1)
 			));
 	}
 }
