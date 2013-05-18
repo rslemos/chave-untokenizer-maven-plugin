c69b3c7e51ff6c51fb8867dd63eb2044ed563f18
R. Lemos <rslemos@gmail.com>
2013-05-18 17:04:47 -0300

    detecta quebra de linha após pontação em pares
    (início e fim de pontuação)

--- src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/NewLineMatchStrategyUnitTest.java
+++ src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/NewLineMatchStrategyUnitTest.java
@@ -71,4 +71,28 @@ public class NewLineMatchStrategyUnitTest extends AbstractMatchStrategyUnitTest 
 				match("sample text?".length(), "sample text?".length(), span("sample text?".length(), "sample text?".length(), 0))
 			);
 	}
+
+	@Test
+	public void testStartPunctuation() throws Exception {
+		cg.add("$¶");
+		runOver("sample text(");
+		
+		verifyMatches(
+				match("sample".length(), "sample".length(), span("sample".length(), "sample".length(), 0)),
+				match("sample text".length(), "sample text".length(), span("sample text".length(), "sample text".length(), 0)),
+				match("sample text(".length(), "sample text(".length(), span("sample text(".length(), "sample text(".length(), 0))
+			);
+	}
+
+	@Test
+	public void testEndPunctuation() throws Exception {
+		cg.add("$¶");
+		runOver("sample text)");
+		
+		verifyMatches(
+				match("sample".length(), "sample".length(), span("sample".length(), "sample".length(), 0)),
+				match("sample text".length(), "sample text".length(), span("sample text".length(), "sample text".length(), 0)),
+				match("sample text)".length(), "sample text)".length(), span("sample text)".length(), "sample text)".length(), 0))
+			);
+	}
 }
