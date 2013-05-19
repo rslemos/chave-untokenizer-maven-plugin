171592e48c9f46d8dbe72456db12027c1e8b63ab
R. Lemos (oslo) <rslemos@gmail.com>
2013-05-19 14:09:56 -0300

    casos que passaram a funcionar com medidas extremas

--- src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/UntokenizerNotWorkingDataFunctionalTest.java
+++ src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/UntokenizerNotWorkingDataFunctionalTest.java
@@ -26,14 +26,5 @@ import org.junit.Test;
 
 @Ignore
 public class UntokenizerNotWorkingDataFunctionalTest extends UntokenizerAbstractFunctionalTest {
-	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$012() { _(); } // 338-th entry: Africa=do=Sul=o'=apartheid'; buffer at 6590\nDump remaining buffer (5026): Africa do Sul o 'apartheid'
-	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$063() { _(); } //23-th entry: Da=Redação; Dump remaining buffer: Sua administração é considerada ruim ou péssima por 41%
-	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$081() { _(); } //212-th entry: Folha; Dump remaining buffer: Se me frustrei é
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$094() { _(); } //2-th entry: dÁgua ALT xxxua; Dump remaining buffer: d'Água' obteve
-	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$099() { _(); } //3-th entry: Brunner; Dump remaining buffer: Seu último trabalho 
-	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$110() { _(); } //34-th entry: Reuniu; Dump remaining buffer: As duas estrelas da
-	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$112() { _(); } //0-th entry: Produtor=de'Concubina; Dump remaining buffer: Produtor de 'Concubina' briga
-	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$114() { _(); } //80-th entry: Time=Warner=Inc.; Dump remaining buffer: - O mais poderoso grupo de comunicação
-	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$122() { _(); } //331-th entry: mudou; Dump remaining buffer: as mulheres se esqueceram de 
-	
 }
--- src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/UntokenizerWorkingDataFunctionalTest.java
+++ src/test/java/br/eti/rslemos/nlp/corpora/chave/parser/UntokenizerWorkingDataFunctionalTest.java
@@ -35,6 +35,7 @@ public class UntokenizerWorkingDataFunctionalTest extends UntokenizerAbstractFun
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$009() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$010() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$011() { _(); }
+	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$012() { _(); } // 338-th entry: Africa=do=Sul=o'=apartheid'; buffer at 6590\nDump remaining buffer (5026): Africa do Sul o 'apartheid'
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$013() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$014() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$015() { _(); }
@@ -85,6 +86,7 @@ public class UntokenizerWorkingDataFunctionalTest extends UntokenizerAbstractFun
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$060() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$061() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$062() { _(); }
+	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$063() { _(); } //23-th entry: Da=Redação; Dump remaining buffer: Sua administração é considerada ruim ou péssima por 41%
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$064() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$065() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$066() { _(); }
@@ -102,6 +104,7 @@ public class UntokenizerWorkingDataFunctionalTest extends UntokenizerAbstractFun
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$078() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$079() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$080() { _(); }
+	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$081() { _(); } //212-th entry: Folha; Dump remaining buffer: Se me frustrei é
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$082() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$083() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$084() { _(); }
@@ -118,6 +121,7 @@ public class UntokenizerWorkingDataFunctionalTest extends UntokenizerAbstractFun
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$096() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$097() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$098() { _(); }
+	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$099() { _(); } //3-th entry: Brunner; Dump remaining buffer: Seu último trabalho 
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$100() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$101() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$102() { _(); }
@@ -128,8 +132,11 @@ public class UntokenizerWorkingDataFunctionalTest extends UntokenizerAbstractFun
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$107() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$108() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$109() { _(); }
+	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$110() { _(); } //34-th entry: Reuniu; Dump remaining buffer: As duas estrelas da
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$111() { _(); }
+	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$112() { _(); } //0-th entry: Produtor=de'Concubina; Dump remaining buffer: Produtor de 'Concubina' briga
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$113() { _(); }
+	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$114() { _(); } //80-th entry: Time=Warner=Inc.; Dump remaining buffer: - O mais poderoso grupo de comunicação
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$115() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$116() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$117() { _(); }
@@ -137,6 +144,7 @@ public class UntokenizerWorkingDataFunctionalTest extends UntokenizerAbstractFun
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$119() { _(); } //293-th entry: O=Onus...>; Dump remaining buffer: O Onus...' até que
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$120() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$121() { _(); }
+	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$122() { _(); } //331-th entry: mudou; Dump remaining buffer: as mulheres se esqueceram de 
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$123() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$124() { _(); }
 	@Test public void test$pt_BR$CHAVEFolha$1994$01$01$125() { _(); }
